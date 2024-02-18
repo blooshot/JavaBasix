e2ada02ec44f68487551209beba93b8c5bd4c2e5
bloodshot <krishna.fnff@gmail.com>
2024-02-18 07:14:37 +0530

    firstCommit

---
+++ src/main/java/org/basix/Main.java
@@ -0,0 +1,9 @@
+package org.basix;
+
+import org.basix.test.Testing;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/basix/test/Testing.java
@@ -0,0 +1,17 @@
+package org.basix.test;
+
+public class Testing {
+    private int Check = 0;
+
+//    public Testing(int check) {
+//        Check = check;
+//    }
+
+    public void setCheck(int check) {
+        Check = check;
+    }
+
+    public int getCheck() {
+        return Check;
+    }
+}
